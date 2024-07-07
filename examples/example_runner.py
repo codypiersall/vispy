92c6b9d2bcb3a954bbdc28cb2df2d308a77766b0
Cody Piersall <cody@ou.edu>
2024-07-07 11:25:36 -0500

    Filter examples.

--- examples/example_runner.py
+++ examples/example_runner.py
@@ -3,12 +3,25 @@ import os
 import subprocess
 import sys
 
-from PyQt6 import QtWidgets, QtCore, QtGui
+from PyQt6 import QtWidgets, QtCore
 
 app = QtWidgets.QApplication([])
 
 THIS_DIR = pathlib.Path(__file__).parent
 
+
+class FileWidget(QtWidgets.QWidget):
+    def __init__(self, files: list[pathlib.Path]):
+        super().__init__()
+        self._search = QtWidgets.QLineEdit()
+        self._search.setPlaceholderText("Filter examples by typing here")
+        self._file_list = FileList(files)
+        self._search.textChanged.connect(self._file_list.filter)
+
+        self._layout = QtWidgets.QVBoxLayout()
+        self._layout.addWidget(self._search)
+        self._layout.addWidget(self._file_list)
+        self.setLayout(self._layout)
 
 class FileList(QtWidgets.QListWidget):
     def __init__(self, files: list[pathlib.Path]):
@@ -29,10 +42,17 @@ class FileList(QtWidgets.QListWidget):
             self._items.append(item)
             self.addItem(item)
 
+    def filter(self, pattern):
+        for row in range(self.count()):
+            item = self.item(row)
+            assert item is not None
+            item.setHidden(pattern not in item.data(0))
+
     def show_thing(self, item: QtWidgets.QListWidgetItem):
         subprocess.Popen([sys.executable, item.data(1)])
 
     def keyPressEvent(self, e):
+        assert e is not None
         super().keyPressEvent(e)
         if e.key() == QtCore.Qt.Key.Key_Return:
             current = self.selectedItems()
@@ -50,7 +70,7 @@ class VispyExampleViewer(QtWidgets.QMainWindow):
         self._main_window = QtWidgets.QMainWindow()
         self._main_window.setMinimumSize(400, 800)
         self.files_dock = QtWidgets.QDockWidget()
-        self.files_widget = FileList(list(THIS_DIR.rglob("*.py")))
+        self.files_widget = FileWidget(list(THIS_DIR.rglob("*.py")))
         policy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Policy.Expanding, QtWidgets.QSizePolicy.Policy.Expanding)
         self.files_widget.setSizePolicy(policy)
         self.files_dock.setWidget(self.files_widget)
